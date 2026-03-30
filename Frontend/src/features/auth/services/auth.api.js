d3dfbcd2c01dcbbba6b50bc7c737e297800dbea3
rushikesh bhagat <rushikesh@gmail.com>
2026-03-30 12:38:41 +0530

    deploy backend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://jobpilot-ai-k3ab.onrender.com",
     withCredentials: true
 })
 
