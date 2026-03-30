c39621164c6e68f4930698c878ef39d0c9887d86
rushikesh bhagat <rushikesh@gmail.com>
2026-03-30 13:10:10 +0530

    landing url

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -4,7 +4,7 @@ import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/interview/pages/Home";
 import Interview from "./features/interview/pages/Interview";
-import LandingPage from "./features/auth/pages/landingPage";
+import LandingPage from "./features/auth/pages/LandingPage";
 
 
 export const router = createBrowserRouter([
