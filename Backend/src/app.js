a12778f98acaf56a7b6e7591523aabd86d5bb533
rushikesh bhagat <rushikesh@gmail.com>
2026-03-30 17:36:12 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "https://jobpilot-ai.vercel.app",
+  origin: "https://job-pilot-ai-w7m4.vercel.app/",
   credentials: true
 }));
 
