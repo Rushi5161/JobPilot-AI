754b304c5260632732966141bdcc57f01319352a
rushikesh bhagat <rushikesh@gmail.com>
2026-03-30 13:04:27 +0530

    Frontend build fixed + env added

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "https://jobpilot-ai-k3ab.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 })
 
