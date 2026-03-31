ef7c4b73ede9e3f139bc0c159c88aa132767a7d4
rushikesh bhagat <rushikesh@gmail.com>
2026-03-31 21:53:04 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://job-pilot-ai-7660.vercel.app/",
   credentials: true
 }));
 
